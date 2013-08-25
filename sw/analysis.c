15dc0c76dfcb67873fe23b509ac4c49ad38d9373
Matthew Peddie <peddie@alum.mit.edu>
2013-08-25 01:28:38 -0700

    a touch of dubstep tuning

--- sw/analysis.c
+++ sw/analysis.c
@@ -157,8 +157,7 @@ float thresholds[NUM_CHANNELS] = {0};
 void
 analyze(float input[NUM_BANDS], float output[NUM_CHANNELS]) {
   /* Simple bin chunking for now */
-  static uint32_t loopcount = 0;
-  output[0] = input[2] + input[3];
+  output[0] = input[1] + input[2] + input[3];
   output[1] = input[4] + input[5];
   output[2] = input[6] + input[7];
   output[3] = input[8] + input[9];
